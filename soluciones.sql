2afd8d930161c1f4bcf5f4423f656e7493e93390
Paulis2029 <paulisbocanegra@gmail.com>
2026-08-11 14:22:31 -0300

    Add SQL queries for product sales and execution times
    Added SQL queries with execution times and results for product sales.

---
+++ soluciones.sql
@@ -0,0 +1,21 @@
+Consulta n.° 1 Tiempo de ejecución: 11,27 ms
+producto_id	producto	categorías	precio	venta_id
+109	Parlante Bluetooth	Audio	60.00	nulo
+108	Hub USB-C de 7 pines	Accesorios	45.00	nulo
+Consulta n.° 2 Tiempo de ejecución: 0,47 ms
+venta_id	producto_inexistente	cantidad	fecha_venta	catálogo_de_productos
+10	999	1	25/03/2024	nulo
+Consulta n.° 3 Tiempo de ejecución: 0,35 ms
+catálogo_prod_id	producto	venta_id	venta_prod_id	fecha_venta
+101	Laptop Pro 15	1	101	5 de enero de 2024
+102	Ratón Inalámbrico	2	102	8 de enero de 2024
+103	Monitor 4K de 27"	3	103	12/01/2024
+101	Laptop Pro 15	4	101	3 de febrero de 2024
+104	Teclado Mecánico	5	104	10/02/2024
+105	Auriculares BT Pro	6	105	18/02/2024
+106	SSD Externo de 1 TB	7	106	5 de marzo de 2024
+102	Ratón Inalámbrico	8	102	12/03/2024
+107	Cámara web HD 1080p	9	107	2024-03-20
+nulo	nulo	10	999	25/03/2024
+109	Parlante Bluetooth	nulo	nulo	nulo
+108	Hub USB-C de 7 pines	nulo	nulo	nulo
